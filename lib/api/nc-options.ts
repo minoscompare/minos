5f96d6c7fca627eed046885b090b47d7935c4248
Benjamin Sengupta <benjaminsengupta@gmail.com>
2022-01-30 17:35:41 -0500

    Remove api index file

--- lib/api/nc-options.ts
+++ lib/api/nc-options.ts
@@ -18,7 +18,7 @@ export const ncOptions: Options<AppApiRequest, AppApiResponse> = {
       }
     }
   },
-  onNoMatch: (req, res) => {
+  onNoMatch: (_req, res) => {
     res.status(404).json({ status: 404, message: 'Route not found' });
   },
 };
