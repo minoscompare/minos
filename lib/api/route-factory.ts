d17ad420eea3f0ec7e52390f224d42a982245e6c
Benjamin Sengupta <benjaminsengupta@gmail.com>
2022-02-01 10:43:26 -0500

    Add route factory

---
+++ lib/api/route-factory.ts
@@ -0,0 +1,31 @@
+import nc from 'next-connect';
+import { AppApiRequest, AppApiResponse } from '@minos/lib/api/types';
+import { Options } from 'next-connect';
+import { isHttpError } from 'http-errors';
+import { ApiError } from 'next/dist/server/api-utils';
+
+export const ncOptions: Options<AppApiRequest, AppApiResponse> = {
+  onError: (err, _req, res) => {
+    if (isHttpError(err) || err instanceof ApiError) {
+      res.status(err.statusCode).json({
+        status: err.statusCode,
+        message: err.message,
+      });
+    } else {
+      console.error(err);
+      if (process.env.NODE_ENV === 'production') {
+        // In production, don't send a stack trace
+        res.status(400).json({ status: 400, message: 'Internal server error' });
+      } else {
+        res.status(400).json({ status: 400, message: err.toString() });
+      }
+    }
+  },
+  onNoMatch: (_req, res) => {
+    res.status(404).json({ status: 404, message: 'Route not found' });
+  },
+};
+
+export default function createHandler() {
+  return nc<AppApiRequest, AppApiResponse>(ncOptions);
+}
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,8 +1,6 @@
-import nc from 'next-connect';
-import { AppApiRequest, AppApiResponse } from '@minos/lib/api/types';
-import { ncOptions } from '@minos/lib/api/nc-options';
+import createHandler from '@minos/lib/api/route-factory';
 
-const handler = nc<AppApiRequest, AppApiResponse>(ncOptions);
+const handler = createHandler();
 
 handler.get((req, res) => {
   res.status(200).json({ name: 'John Doe' });
