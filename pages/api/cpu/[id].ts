a6d330641c897aa82f60e70f20f857bdb811761a
Benjamin Sengupta <benjaminsengupta@gmail.com>
2022-02-04 22:53:11 -0500

    Add cpu GET by id

--- pages/api/cpu/[id].ts
+++ pages/api/cpu/[id].ts
@@ -2,8 +2,19 @@ import createHandler from '@minos/lib/api/create-handler';
 import { validateBodySchema } from '@minos/lib/api/middlewares/validate-schema';
 import { CpuSchema, FromSchema } from '@minos/lib/api/schemas';
 import prisma from '@minos/lib/prisma';
+import createHttpError from 'http-errors';
 
 const handler = createHandler();
+
+handler.get(async (req, res) => {
+  const id = req.query.id as string;
+
+  const cpu = await prisma.cpu.findUnique({ where: { id } });
+
+  if (!cpu) throw new createHttpError.NotFound();
+
+  res.status(200).json({ data: cpu });
+});
 
 const CpuPutBodySchema = CpuSchema;
 
