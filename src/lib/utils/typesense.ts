953f3e842b9f853e9394afab2d5d9c616f61f637
Benjamin Sengupta <benjaminsengupta@gmail.com>
2022-03-07 12:10:07 -0500

    Fix typesense error

--- src/lib/utils/typesense.ts
+++ src/lib/utils/typesense.ts
@@ -6,19 +6,19 @@ const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
     nodes: [
       {
         host: process.env.NEXT_PUBLIC_TYPESENSE_HOST!,
-        port: process.env.NEXT_PUBLIC_TYPESENSE_PORT!,
+        port: parseInt(process.env.NEXT_PUBLIC_TYPESENSE_PORT!),
         protocol: process.env.NEXT_PUBLIC_TYPESENSE_PROTOCOL! as
           | 'https'
           | 'http',
       },
     ],
+    cacheSearchResultsForSeconds: 2 * 60, // Cache search results from server. Defaults to 2 minutes. Set to 0 to disable caching.
   },
-  cacheSearchResultsForSeconds: 2 * 60, // Cache search results from server. Defaults to 2 minutes. Set to 0 to disable caching.
   // The following parameters are directly passed to Typesense's search API endpoint.
   //  So you can pass any parameters supported by the search endpoint below.
   //  queryBy is required.
   additionalSearchParameters: {
-    queryBy: 'fullName',
+    query_by: 'fullName',
   },
 });
 
