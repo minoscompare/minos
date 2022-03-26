9e57a2399097fa81dd34c9f164b00ad73d8bb254
Benjamin Sengupta <benjaminsengupta@gmail.com>
2022-03-26 14:35:52 -0400

    Update favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,27 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0566ac" />
+        <meta name="msapplication-TileColor" content="#0566ac" />
+        <meta name="theme-color" content="#ffffff" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <JotaiProvider>
